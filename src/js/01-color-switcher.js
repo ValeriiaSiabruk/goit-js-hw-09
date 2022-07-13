364c07f5b32c04a53b3bfb36a14fbd0ff154eb32
Valeriia Siabruk <vsyabryk@gmail.com>
2022-07-13 13:13:09 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  if (timerId !== null) return;
+
+  body.style.backgroundColor = getRandomHexColor();
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  timerId = null;
+});
